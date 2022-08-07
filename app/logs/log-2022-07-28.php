dccbcffb18bd4bcb36addb5487f82ca986f501aa
kimheng <kimheng@gmail.com>
2022-08-07 13:40:38 +0700

    invoice

---
+++ app/logs/log-2022-07-28.php
@@ -0,0 +1,14 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2022-07-28 09:07:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) D:\XAMP\htdocs\invoice\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-07-28 09:07:37 --> Unable to connect to the database
+ERROR - 2022-07-28 09:07:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) D:\XAMP\htdocs\invoice\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-07-28 09:07:37 --> Severity: Warning --> session_start(): Failed to initialize storage module: user (path: D:\XAMP\tmp) D:\XAMP\htdocs\invoice\system\libraries\Session\Session.php 143
+ERROR - 2022-07-28 09:07:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) D:\XAMP\htdocs\invoice\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-07-28 09:07:37 --> Unable to connect to the database
+ERROR - 2022-07-28 09:07:37 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SET SESSION sql_mode = ""
+ERROR - 2022-07-28 09:07:37 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'dbuser'@'localhost' (using password: YES) D:\XAMP\htdocs\invoice\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2022-07-28 09:07:37 --> Unable to connect to the database
+ERROR - 2022-07-28 09:07:37 --> Query error: Access denied for user 'dbuser'@'localhost' (using password: YES) - Invalid query: SELECT *
+FROM `sma_settings`
+ERROR - 2022-07-28 09:07:37 --> Severity: error --> Exception: Call to a member function num_rows() on bool D:\XAMP\htdocs\invoice\app\models\Site.php 305
